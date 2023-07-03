16c49a22277e07c9bfda5544f2e520ce02f9e9b4
Jerico2530 <124427215+Jerico2530@users.noreply.github.com>
2023-07-03 17:26:16 +0000

    modificacion

--- src/main/java/com/example/AlfredJerico/Controller.java
+++ src/main/java/com/example/AlfredJerico/Controller.java
@@ -1,8 +1,10 @@
 package com.example.AlfredJerico;
+
 import java.lang.String;
 import  org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+
 @RestController
 @RequestMapping(path="/")
 public class Controller{
